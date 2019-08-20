c5cc239a51b6de8a5b77326de0e2820447fe39f5
Alexander Kuzmenkov <akuzm@yandex-team.ru>
2019-08-20 20:38:56 +0300

    string hash tables -- it compiles somehow

--- dbms/src/Common/HashTable/HashTable.h
+++ dbms/src/Common/HashTable/HashTable.h
@@ -277,6 +277,9 @@ protected:
     template <typename, typename, typename, typename, typename, typename, size_t>
     friend class TwoLevelHashTable;
 
+    template <typename, typename, size_t>
+    friend class TwoLevelStringHashTable;
+
     using HashValue = size_t;
     using Self = HashTable;
     using cell_type = Cell;
